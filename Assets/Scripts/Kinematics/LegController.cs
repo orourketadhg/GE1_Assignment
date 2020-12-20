7efedacb2f851b587b7c76e50f481483bbe578eb
Tadhg O'Rourke <orourketadhg@gmail.com>
2020-12-20 14:19:52 +0000

    Walker body work

--- Assets/Scripts/Kinematics/LegController.cs
+++ Assets/Scripts/Kinematics/LegController.cs
@@ -1,18 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class LegController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+namespace com.GE1Assignment.Kinematics {
+
+    public class LegController : MonoBehaviour {
+
+        public Transform target;
+        public float movementThreshold;
+        public float speed;
+
+        public bool enableMovement;
+
+        private void Update() {
+
+            if (( transform.position - target.position ).sqrMagnitude >= movementThreshold * movementThreshold) {
+                enableMovement = true;
+            }
+            else if (( transform.position - target.position ).sqrMagnitude >= 0.001f * 0.001f) {
+                enableMovement = false;
+            }
+
+            if (enableMovement) {
+                Step();
+            }
+
+        }
+
+        private void Step() {
+            
+        }
+
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
