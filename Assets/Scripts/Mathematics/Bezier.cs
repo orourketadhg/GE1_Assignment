1214501dc9bcd5b82f74f1e87e6e71543028d98d
Tadhg O'Rourke <orourketadhg@gmail.com>
2020-12-21 17:31:44 +0000

    rename

--- Assets/Scripts/Mathematics/Bezier.cs
+++ Assets/Scripts/Mathematics/Bezier.cs
@@ -1,17 +1,21 @@
 ﻿using UnityEngine;
 
-public static class Curves {
+namespace com.GE1Assignment.Mathematics {
 
-    public static Vector3 QuadraticCurve(Vector3 a, Vector3 b, Vector3 c, float t) {
-        Vector3 p0 = Vector3.Lerp(a, b, t);
-        Vector3 p1 = Vector3.Lerp(b,c, t);
-        return Vector3.Lerp(p0, p1, t);
-    }
+    public static class Bezier {
 
-    public static Vector3 CubicCurve(Vector3 a, Vector3 b, Vector3 c, Vector3 d, float t) {
-        Vector3 p0 = QuadraticCurve(a, b, c, t);
-        Vector3 p1 = QuadraticCurve(b, c, d, t);
-        return Vector3.Lerp(p0, p1, t);
-    }
+        public static Vector3 QuadraticCurve(Vector3 a, Vector3 b, Vector3 c, float t) {
+            Vector3 p0 = Vector3.Lerp(a, b, t);
+            Vector3 p1 = Vector3.Lerp(b,c, t);
+            return Vector3.Lerp(p0, p1, t);
+        }
+
+        public static Vector3 CubicCurve(Vector3 a, Vector3 b, Vector3 c, Vector3 d, float t) {
+            Vector3 p0 = QuadraticCurve(a, b, c, t);
+            Vector3 p1 = QuadraticCurve(b, c, d, t);
+            return Vector3.Lerp(p0, p1, t);
+        }
     
+    }
+
 }
