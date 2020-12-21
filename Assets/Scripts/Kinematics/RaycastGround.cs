6dc4a14df29528838846b84a5cc33c1da7c9667b
Tadhg O'Rourke <orourketadhg@gmail.com>
2020-12-21 11:51:55 +0000

    Raycasting feet to ground

---
+++ Assets/Scripts/Kinematics/RaycastGround.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace com.GE1Assignment.Kinematics {
+
+    public class RaycastGround : MonoBehaviour {
+
+
+        public Transform raycastOrigin;
+
+        private void FixedUpdate() {
+
+            if (Physics.Raycast(raycastOrigin.position, Vector3.down, out RaycastHit hit, Mathf.Infinity, 1 << 8)) {
+                transform.position = hit.point;
+            }
+        }
+        
+    }
+
+}
